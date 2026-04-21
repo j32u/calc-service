e625f16576e9c63c707eeb8a995f7189cae6f100
j3w <ansicgolan@gmail.com>
2026-04-21 19:34:32 +0200

    init; bean w Springu

---
+++ src/main/java/pl/example/calc/CalcConfiguration.java
@@ -0,0 +1,17 @@
+package pl.example.calc;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CalcConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+        return objectMapper;
+    }
+}
